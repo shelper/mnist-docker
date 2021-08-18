369ccbc8c47a633c3ac03cf3ba9f73704a3299fb
zhijia yuan <zhijia.yuan@zebra.com>
2021-08-18 09:38:46 -0400

    add gpu and cpu run script separately

--- basic/config.sh
+++ basic/config.sh
@@ -2,6 +2,6 @@
 
 docker_image_name="mnist-learning-basic"
 docker_image_version="latest"
-docker_repository="erikjandevries"
+docker_repository="shelper"
 
 docker_image_id="${docker_repository}/${docker_image_name}:${docker_image_version}"
---
+++ basic/cpu_run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DOCKER_COMMAND=$1
+
+source config.sh
+
+echo "Granting root user access to X display"
+xhost +si:localuser:root
+
+echo "Running Docker image: ${docker_image_id}"
+docker run -it --rm \
+    --name ${docker_image_name} \
+    --volume /tmp/.X11-unix:/tmp/.X11-unix \
+    --env="DISPLAY" \
+    --device=/dev/dri:/dev/dri \
+    --volume /mnt/data/Data/mnist:/mnt/data/Data/mnist \
+    --workdir /repositories/mnist-learning \
+    ${docker_image_id} ${DOCKER_COMMAND}
+
+echo "Revoking root user access to X display"
+xhost -si:localuser:root
